870257fd248b9983d49df01c7e9dde4f839fb51b
opcode02 <>
2003-05-25 21:01:23 +0000

    Header file for the SMBHandler class.

---
+++ MAMEoX/Includes/SMBHandler.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "smb++.h"
+#include <XTL.h>
+#include "StdString.h"
+#include <Vector>
+#include <StdIO.h>
+
+typedef INT64 offset_t;
+
+class CSMBHandler
+{
+public:
+  CSMBHandler(void);
+  virtual ~CSMBHandler(void);
+  bool GetDirectory(const CStdString& strDirectory, const CStdString& strExtFilter, std::vector<CStdString>& vecFiles, BOOL  = FALSE);
+  bool CopyFile(const CStdString& strSrcFile, const CStdString& strDestFile);
+
+  virtual void Close();
+  virtual offset_t Seek(offset_t iFilePosition, int iWhence=SEEK_SET);
+  virtual unsigned int Read(void* lpBuf, offset_t uiBufSize);
+  virtual bool Open(const char* strFullPath);
+  virtual offset_t GetLength();
+  virtual offset_t GetPosition();
+
+protected:
+  void ParsePasswd(const CStdString& strSMBPath);
+
+private:
+  SMB* m_pSMB;
+  offset_t m_fileSize;
+  int m_fd;
+};
