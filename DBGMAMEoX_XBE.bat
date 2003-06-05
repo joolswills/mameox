c780d616be5dfd24e0135d1257ec136c6a5e8844
eabair <>
2003-06-05 20:09:39 +0000

    Removed unused batch files

--- DBGMAMEoX_XBE.bat
+++
@@ -1,7 +0,0 @@
-@echo Creating MAMEoX Debug XBE file
-imagebld.exe @..\imageblddbg.switch
-
-
-@echo Copying file to XBOX
-xbecopy.exe "Debug\MAMEoX.XBE" "xe:\MAMETEST\MAMEoX.xbe" /nologo 
-
--- MAMEoX_XBE.bat
+++
@@ -1,7 +0,0 @@
-@echo Creating MAMEoX Release XBE file
-imagebld.exe @..\imagebld.switch
-
-
-@echo Copying file to XBOX
-xbecopy.exe "Release\MAMEoX.XBE" "xe:\MAMETEST\MAMEoX.xbe" /nologo 
-
