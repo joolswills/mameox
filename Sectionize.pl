297fce5a1f0c621711f8bf956cd63fedfac6f981
eabair <>
2003-06-23 18:47:40 +0000

    Fixed more XBE dep's

--- Sectionize.pl
+++ Sectionize.pl
@@ -338,6 +338,7 @@ print "Sectionizing drivers, sound hardware, and video hardware...\n";
 #  LoadDriverSectionByName( "src\\drivers\\leland.c" );    // Asylum (prototype)
 #  LoadDriverSectionByName( "src\\drivers\\trackfld.c" );  // Hyper Sports, Hyper Olympics '84?
 #  taito_f3 - Super Chase
+#  fromance - Pipe Dream
 
 @SkipDrivers = ( "jrcrypt.c", "mpatrol.c", "snk.c", 
                  "galaxian.c", "scramble.c", "scobra.c", 
@@ -345,7 +346,7 @@ print "Sectionizing drivers, sound hardware, and video hardware...\n";
 	             "espial.c", "bzone.c", "nova2001.c", "gottlieb.c", 
 	             "pengo.c",  "megasys1.c", "rallyx.c", "timeplt.c", 
 				 "exidy.c", "m72.c", "leland.c", "trackfld.c",
-				 "taito_f3.c" );
+				 "taito_f3.c", "fromance.c" );
 
 @CapcomFamily = ( "cps1.c", "cps2.c" );
 
