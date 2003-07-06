8a58ac5d96e565b6dfe46c1720bd0c00bf83fd11
eabair <>
2003-07-06 16:34:45 +0000

    Added script to dump the GAME macros from the driver files to aid in figuring out what driver provides what ROM

---
+++ DumpGamesForEachDriver.pl
@@ -0,0 +1,20 @@
+#!/bin/perl
+
+local @FILEs = `find ./MAME/src/drivers/*.c`;
+
+
+foreach( @FILEs ) {
+
+	$DriverFile = $_;
+	chomp( $DriverFile );
+	open( FILE, "<$DriverFile" ) || die "Failed to open file $DriverFileName!\n";
+	
+	print "--- $DriverFile ---------------------------------------------\n";
+	while( <FILE> ) {
+		if( $_ =~ /GAME.*\(.*,.*,.*,.*,.*,.*,.*,.*,.*\"(.*)\",?.*\)/ ) {
+			print "$1\n";
+		}
+	}
+	close( FILE );
+}
+
